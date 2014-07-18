cb977b9c76e998bee0747d069187cf74eb4b4a7d
Samujjal Purkayastha <me@samujjal.com>
2014-07-18 14:20:44 +0530

    Finish layout and routes

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+
+  let(:base_title) {"ROR Sample App"}
+
+  subject { page }
+
+  describe "Home page" do
+    before { visit root_path}
+
+    it { should have_content('Sample App')}
+    it { should have_title(full_title(''))}
+    it { should_not have_title("| Home")}
+  end
+
+  describe "Help page" do
+    before {visit help_path}
+
+    it { should have_content('Help')}
+    it { should have_title("#{base_title} | Help")}
+  end
+
+  describe "About page" do
+    before {visit about_path}
+
+    it { should have_content('About')}
+    it { should have_title("#{base_title} | About")}
+  end
+
+  describe "Contact page" do
+    before {visit contact_path}
+
+    it { should have_content('Contact')}
+    it { should have_title("#{base_title} | Contact")}
+  end
+end
