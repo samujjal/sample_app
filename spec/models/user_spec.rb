9c12056a4536d3ff79605a4609d4cd0053117aff
Samujjal Purkayastha <me@samujjal.com>
2014-08-02 06:36:31 +0530

    Finished user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,7 +19,7 @@ describe User do
 
   it {should be_valid}
 
-  describe "when name is not present" do
+  describe "when name is too long" do
     before { @user.name = "a" * 51}
     it { should_not be_valid}
   end
@@ -32,7 +32,7 @@ describe User do
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.foo@baz.com
-        foo@bar+baz.com]
+        foo@bar+baz.com foo@bar..com]
         addresses.each do |invalid_address|
           @user.email = invalid_address
           expect(@user).not_to be_valid
@@ -49,6 +49,17 @@ describe User do
       end
     end
   end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.Com" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
 
   describe "when email address is already taken" do
     before do
