0460dad261d55232ea2e860dfb1a57c5f6c96014
Samujjal Purkayastha <me@samujjal.com>
2014-08-07 07:09:21 +0530

    Used form_tag instead of form_for

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by(email: params[:email].downcase)
+    if user && user.authenticate(params[:password])
         sign_in user
         redirect_to user
     else
@@ -12,6 +12,16 @@ class SessionsController < ApplicationController
         render 'new'
     end
   end
+  # def create
+  #   user = User.find_by(email: params[:session][:email].downcase)
+  #   if user && user.authenticate(params[:session][:password])
+  #       sign_in user
+  #       redirect_to user
+  #   else
+  #       flash.now[:error] = "Invalid email/password combination"
+  #       render 'new'
+  #   end
+  # end
 
   def destroy
     sign_out
