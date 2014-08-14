90b3fe810f9f49838bc72f788cd734f2addcf50e
Samujjal Purkayastha <me@samujjal.com>
2014-08-14 23:25:03 +0530

    finished ch 9 and exercises

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -14,7 +14,7 @@ def invalid_signup
   fill_in "Name", with: ""
   fill_in "Email", with: "user@example,com"
   fill_in "Password", with: "foobar"
-  fill_in "Confirmation", with: "notfoobar"
+  fill_in "Confirm Password", with: "notfoobar"
   click_button submit
 end
 
@@ -22,13 +22,21 @@ def valid_signup
   fill_in "Name",         with: "Example User"
   fill_in "Email",        with: "user@example.com"
   fill_in "Password",      with: "foobar"
-  fill_in "Confirmation",  with: "foobar"
+  fill_in "Confirm Password",  with: "foobar"
 end
 
-def valid_signin(user)
-  fill_in "Email",    with: user.email
-  fill_in "Password", with: user.password
-  click_button "Sign in"
+def valid_signin(user, options={})
+  if options[:no_capybara]
+    #Sign in when not using capybara
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.digest(remember_token))
+  else
+    visit signin_path
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+  end
 end
 
 RSpec::Matchers.define :have_error_message do |message|
